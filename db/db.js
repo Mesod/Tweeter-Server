3e09a65446f4fe9b6d9bd33ac02f4b39707a7771
Masood <adelimasood96@gmail.com>
2017-07-26 23:56:26 +0430

    making the register function, unique username and hashed password with bcrypt

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@
  * Created by masood on 7/26/17.
  */
 var mongoose = require('mongoose');
-
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/tweeter');
 
 module.exports = mongoose.connection;
--- db/usersdb.js
+++ db/usersdb.js
@@ -2,26 +2,34 @@
  * Created by masood on 7/26/17.
  */
 var userSchema = require('../schemas/user_schema');
+var bcrypt = require('bcrypt');
 
-var uniqueUserName = function(username) {
+var uniqueUserName = function(username,callback) {
     userSchema.count({userName: username}, function(err, count) {
         if(count>0) {
-            return false;
+            callback(false);
         } else {
-            return true;
+            callback(true);
         }
     })
 }
 
-exports.newUser = function(userInfo, callback) {
-    if(uniqueUserName(userInfo.userName)) {
-        var tempUser = new userSchema(userInfo);
-        tempUser.save(function(err) {
-            if(err) callback("db error");
-            else    callback(null);
-        })
-    } else {
-        callback("invalid username");
-    }
+module.exports.newUser = function(userInfo, callback) {
+    uniqueUserName(userInfo.userName,function(bool) {
+        if(bool) {
+            console.log('runs here');
+            var tempUser = new userSchema(userInfo);
+            bcrypt.genSalt(10,function(err,salt) {
+                bcrypt.hash(tempUser.password,salt,function(err,hash) {
+                    tempUser.password = hash;
+                    tempUser.save(callback);
+                })
+            })
+        } else {
+            console.log('runs 2');
+            callback("invalid username!");
+        }
+    })
+
 }
 
